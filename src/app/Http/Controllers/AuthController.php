3cc1e24a4c0bac53ecad888909fb49aa34a1bd6d
abderrazakerrifaouy <abdrzakrifawi@gmail.com>
2026-02-25 13:06:15 +0000

    Modified src/app/Http/Controllers/AuthController.php at 2026-02-25 13:06:15

--- src/app/Http/Controllers/AuthController.php
+++ src/app/Http/Controllers/AuthController.php
@@ -41,13 +41,28 @@ class AuthController extends Controller
         ]);
         Auth::login($user);
         return redirect()->route('dashboard');
-
     }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            return redirect()->intended('dashboard');
+        }
+
+        return back()->withErrors([
+            'email' => 'The provided credentials do not match our records.',
+        ]);
+    }
+
     public function logout()
     {
         Auth::logout();
         return redirect()->route('login');
     }
-
 
 }

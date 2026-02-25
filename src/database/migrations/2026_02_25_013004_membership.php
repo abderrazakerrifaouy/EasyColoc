fef376eec744bf98e7b1fe0bb927e6e6f834c0f8
abderrazakerrifaouy <abdrzakrifawi@gmail.com>
2026-02-25 02:24:20 +0000

    Added content to src/database/migrations/2026_02_25_013004_membership.php at 2026-02-25 02:24:20

---
+++ src/database/migrations/2026_02_25_013004_membership.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('memberships', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('group_id')->constrained()->onDelete('cascade');
+            $table->enum('role', ['owner', 'member'])->default('member');
+            $table->boolean('is_approved')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};

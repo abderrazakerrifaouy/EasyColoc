8e6d51860b49440d3006b5e51a8346eea0b7cac3
abderrazakerrifaouy <abdrzakrifawi@gmail.com>
2026-02-25 02:24:33 +0000

    Added content to src/database/migrations/2026_02_25_014651_payment.php at 2026-02-25 02:24:33

---
+++ src/database/migrations/2026_02_25_014651_payment.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('invoice_id')->constrained()->onDelete('cascade');
+            $table->foreignId('payer_membership_id')->constrained('memberships')->onDelete('cascade');
+            $table->foreignId('receiver_membership_id')->constrained('memberships')->onDelete('cascade');
+            $table->double('amount');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};

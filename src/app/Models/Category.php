1adc898a804fda4421e87cb0926d28cac989573b
abderrazakerrifaouy <abdrzakrifawi@gmail.com>
2026-02-25 02:01:31 +0000

    Added content to src/app/Models/Category.php at 2026-02-25 02:01:31

---
+++ src/app/Models/Category.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    protected $fillable = ['name', 'group_id'];
+
+    public function group()
+    {
+        return $this->belongsTo(Group::class);
+    }
+
+    public function invoices()
+    {
+        return $this->hasMany(Invoice::class);
+    }
+    
+}

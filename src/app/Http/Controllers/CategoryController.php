ac75e093e341c7bfc31989d9001b96cdb843a39e
abderrazakerrifaouy <abdrzakrifawi@gmail.com>
2026-02-26 10:44:13 +0000

    Added content to src/app/Http/Controllers/CategoryController.php at 2026-02-26 10:44:13

---
+++ src/app/Http/Controllers/CategoryController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    public function create(Request $request){
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'group_id' => 'required|exists:groups,id',
+        ]);
+        Category::created(
+            [
+                'name' => $request->name,
+                'group_id' => $request->group_id,
+            ]
+        );
+        return redirect()->back()->with('success', 'Category created successfully!');
+    }
+}

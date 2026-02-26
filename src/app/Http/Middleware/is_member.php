b16a9eff47de287b31e0c1dbd750ca09102631b6
abderrazakerrifaouy <abdrzakrifawi@gmail.com>
2026-02-26 10:44:27 +0000

    Added content to src/app/Http/Middleware/is_member.php at 2026-02-26 10:44:27

---
+++ src/app/Http/Middleware/is_member.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class is_member
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $user = Auth::user();
+        if ($user->role == 'user') {
+            $membership = $user->memberships()->where('is_approved', false)->first();
+            if (!$membership || $membership->role != 'member') {
+                return redirect()->route('dashboard');
+            } else {
+                return $next($request);
+            }
+        } else {
+            return redirect()->route('dashboardAdmin');
+        }
+    }
+}
